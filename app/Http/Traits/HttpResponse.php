75ac347ce183cf607814225acf6d2872177bc880
Rence Salaveria <rencesalaveria.dev@gmail.com>
2024-10-19 15:00:22 +0800

    Initial Changes

---
+++ app/Http/Traits/HttpResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Enums\HttpStatus;
+
+trait HttpResponse
+{
+    public function success($data, $message, $code = HttpStatus::OK) {
+        return response()->json([
+            'status' => "Request successful",
+            'message' => $message,
+            'data' => $data,
+            'requestTime' => now()
+        ], $code->value);
+    }
+
+    protected function error($message, HttpStatus $code): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'message' => $message,
+        ], $code->value);
+    }
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullName');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
