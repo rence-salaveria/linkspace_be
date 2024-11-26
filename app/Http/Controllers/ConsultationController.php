80388ff6266cf64d8c2cf7809d4c00d0d7e159e8
Rence Salaveria <cbsalaveria@ncmh.gov.ph>
2024-11-26 15:21:00 +0800

    fix (Consultations) Display proper data in consultations

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\HttpStatus;
 use App\Http\Requests\ConsultationRequest;
 use App\Http\Resources\ConsultationResource;
 use App\Http\Traits\AuditLogger;
 use App\Http\Traits\HttpResponse;
 use App\Models\Consultation;
+use Illuminate\Http\Request;
 
 class ConsultationController extends Controller
 {
@@ -22,10 +24,31 @@ class ConsultationController extends Controller
         return new ConsultationResource(Consultation::create($request->validated()));
     }
 
-    public function getByCounselorId(int $counselor)
+    public function getByCounselorId(int $counselor, Request $request)
     {
-        $allConsultations = Consultation::with(['student'])->where('counselor_id', $counselor)->get();
-        return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
+        // type = pending, upcoming, done
+        if ($request->type === "pending") {
+            $allConsultations = Consultation::with(['student'])
+                ->where('counselor_id', $counselor)
+                ->whereIn('status', ['LookUp-001', 'LookUp-002'])
+                ->get();
+            return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
+        } elseif ($request->type === "upcoming") {
+            $allConsultations = Consultation::with(['student'])
+                ->where('counselor_id', $counselor)
+                ->where('status', 'LookUp-002')
+                ->whereDate('schedule_date', now()->toDateString())
+                ->get();
+            return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
+        } elseif ($request->type === "done") {
+            $allConsultations = Consultation::with(['student'])
+                ->where('counselor_id', $counselor)
+                ->where('status', 'LookUp-003')
+                ->get();
+            return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
+        } else {
+            return $this->error("Invalid type", HttpStatus::BAD_REQUEST);
+        }
     }
 
     public function show(int $consultationID)
