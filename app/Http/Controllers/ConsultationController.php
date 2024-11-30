e94a1f45a275cad5a7cf98202649cf9173ccc5e1
Rence Salaveria <cbsalaveria@ncmh.gov.ph>
2024-11-30 15:49:40 +0800

    feat (Consultations) Can now access add consultations properly

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -7,12 +7,13 @@ use App\Http\Requests\ConsultationRequest;
 use App\Http\Resources\ConsultationResource;
 use App\Http\Traits\AuditLogger;
 use App\Http\Traits\HttpResponse;
+use App\Http\Traits\UserInfoAccess;
 use App\Models\Consultation;
 use Illuminate\Http\Request;
 
 class ConsultationController extends Controller
 {
-    use HttpResponse, AuditLogger;
+    use HttpResponse, AuditLogger, UserInfoAccess;
 
 //    public function index()
 //    {
@@ -31,19 +32,22 @@ class ConsultationController extends Controller
             $allConsultations = Consultation::with(['student'])
                 ->where('counselor_id', $counselor)
                 ->whereIn('status', ['LookUp-001', 'LookUp-002'])
+                ->orderBy('created_at', 'desc')
                 ->get();
             return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
         } elseif ($request->type === "today") {
             $allConsultations = Consultation::with(['student'])
                 ->where('counselor_id', $counselor)
                 ->where('status', 'LookUp-002')
+                ->orderBy('created_at', 'desc')
                 ->whereDate('schedule_date', now()->toDateString())
                 ->get();
             return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
         } elseif ($request->type === "done") {
             $allConsultations = Consultation::with(['student'])
                 ->where('counselor_id', $counselor)
-                ->where('status', 'LookUp-003')
+                ->whereIn('status', ['LookUp-003', 'LookUp-004'])
+                ->orderBy('created_at', 'desc')
                 ->get();
             return $this->success(ConsultationResource::collection($allConsultations), "Fetched successfully");
         } else {
@@ -54,5 +58,30 @@ class ConsultationController extends Controller
     public function show(int $consultationID)
     {
         return new ConsultationResource(Consultation::with(['student'])->findOrFail($consultationID));
+    }
+
+    public function create(Request $request)
+    {
+        $counselorId = $this->getUserId($request);
+        $status = "";
+
+        if ($request->scheduleDate !== null) {
+            $status = "LookUp-002";
+            $request->scheduleDate = date('Y-m-d H:i:s', strtotime($request->scheduleDate));
+            Consultation::create([
+                'counselor_id' => $counselorId,
+                'student_id' => $request->selectedStudent,
+                'schedule_date' => $request->scheduleDate,
+                'status' => $status,
+            ]);
+        } else {
+            $status = "LookUp-001";
+            Consultation::create([
+                'counselor_id' => $counselorId,
+                'student_id' => $request->selectedStudent,
+                'schedule_date' => null,
+                'status' => $status,
+            ]);
+        }
     }
 }
