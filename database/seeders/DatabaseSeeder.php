44474f3be714764172e955f694e98df0eccaf225
Clarence Rhey Salaveria <rencesalaveria.dev@gmail.com>
2024-12-10 12:49:23 +0800

    Remove seeding for Student and Consultation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
 //            'email' => 'test@example.com',
 //        ]);
         $this->call(UserSeeder::class);
-        $this->call(StudentSeeder::class);
-        $this->call(ConsultationSeeder::class);
+        // $this->call(StudentSeeder::class);
+        // $this->call(ConsultationSeeder::class);
     }
 }
